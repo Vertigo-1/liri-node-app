80e035597cc15fde4930d19cf860fd46a970fc1a
Vertigo-1 <46118156+Vertigo-1@users.noreply.github.com>
2019-03-26 19:02:58 -0400

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log("this is loaded");
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: 2b932471785d4c99bbca935dfacfb031,
+  secret: 13dfb2d863ff4dd7ba8c12ce65201155
 };
